22687ab6e57a80af6824ba58662e80b5142497e7
Nick Freear <nfreear@yahoo.co.uk>
2016-03-18 12:27:52 +0000

    Bug #81, Add `Gruntfile`, Travis-CI, `npm-install` to Composer [iet:5388828]
    * https://github.com/IET-OU/open-media-player/issues/81

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function (grunt) {
+	'use strict';
+
+	grunt.initConfig({
+		csslint: {
+			lax: {
+				options: {
+          quiet: true  // Ignore warnings.
+			  },
+				src: 'themes/**/*.css'
+			},
+			strict: 'themes/**/*.css'
+		},
+		jshint: {
+			options: {
+				eqeqeq: true,
+				laxcomma: true,
+				//'-W097': true,  // Ignore position of 'use strict';
+				//'-W100': true,  // ??
+				//'-W014': true,  // Ignore bad line breaking before '+';
+				globals: {
+					// None!
+				},
+				ignores: [
+					'**/mep-feature-fullscreen.js',
+					'**/mep-feature-tracks.js',
+					'**/mep-player.js',
+					'**/*.min.js'
+				]
+			},
+			themes: 'themes/**/*.js',
+			grunt:  'Gruntfile.js'
+		},
+
+		nggettext_extract: { },
+		nggettext_compile: { },
+		msgInitMerge: { },
+		sass: { }
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
+
+	grunt.registerTask('default', [ 'csslint:lax', 'jshint' ]);
+};
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
+[![Build status][travis-icon]][travis-ci]
+
+
 # IET-OU / open-media-player-themes
 
 Player themes for the [Open Media Player][omp].
 
-* http://embed.open.ac.uk
+* <https://embed.open.ac.uk>
 
 
 ## Installation
@@ -28,3 +31,5 @@ License:  [MIT][]
 [iet-logo]: http://www.gravatar.com/avatar/94fd203f6c3a056cf590a76ea2f03387?t=IET&s=40
 [ou]:  http://www.open.ac.uk/
 [Composer]: https://getcomposer.org/
+[travis-ci]: https://travis-ci.org/IET-OU/open-media-player-themes "Build status — Travis CI"
+[travis-icon]: https://travis-ci.org/IET-OU/open-media-player-themes.svg
